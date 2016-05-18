c4cd766fae3b9694e44ecf5e674ac7f95e584c94
Laila Bougria <laila.bougria@gmail.com>
2016-05-18 22:13:46 +0200

    no message

---
+++ Sample/MyService.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyService
+{
+    static ILog logger = LogManager.GetLogger<MyService>();
+    
+    public void WriteHello()
+    {
+        logger.Info("Hello from MyService.");
+    }
+}
+
+public interface ITesterke
+{
+}
+
+public class Testerke : ITesterke
+{
+    readonly IBus bus;
+
+    public Testerke(IBus bus)
+    {
+        this.bus = bus;
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.Title = "Samples.Unity";
+        #region ContainerConfiguration
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Test");
+        busConfiguration.UseTransport<AzureServiceBusTransport>().ConnectionString("xxx");
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        //busConfiguration.License(ConfigurationService.Instance.NServiceBusLicense);
+        //LogManager.UseFactory(new NServiceBusToStackifyLoggerFactory());
+
+        UnityContainer container = new UnityContainer();
+        container.RegisterInstance(new MyService());
+        container.RegisterType<ITesterke, Testerke>();
+        busConfiguration.UseContainer<UnityBuilder>(c => c.UseExistingContainer(container));
+        #endregion
+
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            bus.SendLocal(new MyMessage());
+            var testerke = container.Resolve<ITesterke>();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
